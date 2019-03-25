229d8b6729144046dd78865d074816aff03c73e4
MaxHermez <Max.Hermez@hotmail.com>
2019-03-25 22:01:29 +0200

    Change output to a list and fix comment typos

--- functions/accesskeys/main.py
+++ functions/accesskeys/main.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 from string import ascii_lowercase, ascii_uppercase
 
+
 def Accesskey(html):
-    """takes a list of all parsed html strings which include interactive html elements
+    """Takes as input a list of all parsed html strings which include interactive html elements
     and adds an "accesskey" attribute to each one"""
     char = list(ascii_lowercase) + list(ascii_uppercase)
+    out = []
     for code in html:  # Iterating over each html string
         # Deserializing the html string
         soup = BeautifulSoup(code, "html.parser")
@@ -13,5 +15,6 @@ def Accesskey(html):
             if not i.has_attr('accesskey'):
                 # Assigns the first available character to the created 'accesskey' value
                 i['accesskey'] = char.pop(0)
-    # .prettify returns a serilized and formatted html string
-    return str(soup.prettify())
+        out.append(str(soup.prettify()))
+    # .prettify returns a list of serilized and formatted html string
+    return out
