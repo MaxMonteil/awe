82af0b84613d689dee25291ef489d61d56d260bf
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-03-30 14:36:58 +0200

    Remove orchestrator as it's job should be done in the main driver file awe.py. Move constants file to root folder.

--- orchestrator/__init__.py
+++
@@ -1,6 +0,0 @@
-from orchestrator import constants
-# from current package(folder) orchestrator, in the module(file) orchestrator
-# import the class Orchestrator
-from orchestrator.orchestrator import Orchestrator
-
-__all__ = ["constants", "Orchestrator"]
