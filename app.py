79ba03ba7145f37368a5b9d4bd1136c13356748d
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-03-31 18:39:48 +0300

    Use Engine class to return parsed audit output.

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-from flask import Flask, request, send_file
+from awe import Engine
+from flask import Flask, request, send_file, jsonify
 import subprocess
 
 app = Flask(__name__)
+ROOT_DIR = "/var/www/awe/"
+OUTPUT_DIR = ROOT_DIR + "/results/"
 
 
 @app.route("/api/")
 def getUrl():
-    page = request.args.get("url", default="", type=str)
-    out_format = request.args.get("output", default="json", type=str)
-    subprocess.call(["sudo", "bash", "/var/www/awe/run.sh", page, out_format], bufsize=0)
-    if (out_format == "html"):
-        return send_file("/var/www/awe/results/result.html")
-    else:
-        subprocess.call(["python", "/var/www/awe/lighthouseparser/testAuditParsing.py"], bufsize=0)
-        return send_file("/var/www/awe/api/testResults.json")
+    target_url = request.args.get("url", default="", type=str)
+    output_format = request.args.get("output", default="json", type=str)
+
+    # Ensure a valid format is given if not, defaults to json
+    if output_format not in ["html", "json"]:
+        output_format = "json"
+
+    OUTPUT_FILE = OUTPUT_DIR + output_format
+
+    subprocess.call(
+        ["sudo", "bash", f"{ROOT_DIR}run.sh", target_url, output_format],
+        bufsize=0,
+    )
+
+    if output_format is "html":
+        return send_file(OUTPUT_FILE)
+
+    audit = ""
+    with open(OUTPUT_FILE, "r") as auditFile:
+        audit = auditFile.read()
+
+    engine = Engine(audit_data=audit)
+
+    return jsonify(engine.lhAudit.get_audit_data()), 200
 
 
 @app.route("/")
