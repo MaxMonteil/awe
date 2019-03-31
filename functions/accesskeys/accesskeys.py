9ac7d17b65b3aa11ba8bf555db71283839c3d348
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-03-31 12:14:50 +0300

    Function main file should have the same name as the function. The driver function in the code must be called 'run'.

--- functions/accesskeys/accesskeys.py
+++ functions/accesskeys/accesskeys.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from string import ascii_lowercase, ascii_uppercase
 
 
-def Accesskey(html):
+def run(html):
     """Takes as input a list of all parsed html strings which include interactive html elements
     and adds an "accesskey" attribute to each one"""
     char = list(ascii_lowercase) + list(ascii_uppercase)
