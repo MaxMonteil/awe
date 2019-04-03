ab84e051041d9e02fdb11f2cf0666b9a5a72df39
MaxHermez <Max.Hermez@hotmail.com>
2019-04-03 20:39:59 +0300

    Adjust input to list of strings

--- awe/functions/accesskeys/accesskeys.py
+++ awe/functions/accesskeys/accesskeys.py
@@ -7,20 +7,22 @@ def run(html):
     Adds accesskey attribute to all interactive HTML elements.
 
     Input:
-        list of HTML strings 
+        List of HTML strings 
+    Output:
+        List of beautiful soup items with proper accesskey attributes
     """
     #to be assigned to accesskey values
     char = list(ascii_lowercase) + list(ascii_uppercase)
     out = []
-    for item in html:
-        soup = BeautifulSoup(item["snippet"], "html.parser")
+    for snippet in html:
+        soup = BeautifulSoup(snippet, "html.parser")
         # Iterates over all html tags in the string
         for i in soup.findAll():
             if i.has_attr('accesskey'):
                 #removes already used accesskey values from the char list
                 char.remove(i['accesskey'][0])
-    for item in html:
-        soup = BeautifulSoup(item["snippet"], "html.parser")
+    for snippet in html:
+        soup = BeautifulSoup(snippet, "html.parser")
         for i in soup.findAll():
             if not i.has_attr('accesskey'):
                 # Assigns the first available character to the created 'accesskey' value
