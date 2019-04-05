9110edc0e8a181127d6c3b87fc8aaa1b2a51f17b
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-04-05 07:10:17 +0300

    Add some styles.

--- site/src/views/About.vue
+++ site/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <h1 class="title">About Us</h1>
+    <p>We're the coolest CMPS 253 team!</p>
   </div>
 </template>
+
+<style scoped>
+#about {
+  margin-top: 64px;
+}
+</style>
