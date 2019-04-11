d616d13f56b326e83c2e0a427cc90d95d5ea5ee9
marina keyrouz <marinakayrouz@gmail.com>
2019-04-11 16:03:02 +0300

    Change output to beautifulsoup objects.

--- awe/functions/image_alt/image_alt.py
+++ awe/functions/image_alt/image_alt.py
@@ -1,13 +1,53 @@
-# Takes json and return html
-import json
+# Takes list of img and return beautifulsoup objects
+from bs4 import BeautifulSoup
+import argparse
+import io
+
+from google.cloud import vision
+from google.cloud.vision import types
 
 
-# read file
-with open('Parse_OLJ.json', 'r') as myfile:
-    data=myfile.read()
+def run(html):
+	 htmlTags = [BeautifulSoup(item["snippet"], "html.parser").find() for item in html]
+	 image(htmlTags)
 
-# parse file
-obj = json.loads(data)
 
-# show values
-print( str(obj['items']['node']['snippet']))
+def image(htmlSnippets):
+	for tag in htmlSnippets:
+			image=tag.find('img')
+			src=tag.find('img').get('src')
+			alt=report(annotate(src))
+			image.set('alt',alt)
+
+def annotate(path):
+    """Returns web annotations given the path to an image."""
+    client = vision.ImageAnnotatorClient()
+
+    if path.startswith('http') or path.startswith('gs:'):
+        image = types.Image()
+        image.source.image_uri = path
+
+    else:
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+
+    web_detection = client.web_detection(image=image).web_detection
+
+    return web_detection
+
+
+def report(annotations):
+    """Prints detected features in the provided web annotations."""
+    alt=""
+    if annotations.pages_with_matching_images:
+        # print('\n{} Pages with matching images retrieved'.format(
+        #     len(annotations.pages_with_matching_images)))
+
+        for entity in annotations.web_entities:
+            alt+=entity.description+" "
+
+            # print('Description: {}'.format(entity.description))
+    return alt
+
