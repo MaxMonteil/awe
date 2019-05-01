400776462bed76341ff9ea13714b6e25c74c282c
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-01 15:48:54 +0300

    First implementation.

--- engine/functions/meta_refresh/meta_refresh.py
+++ engine/functions/meta_refresh/meta_refresh.py
@@ -1,6 +1,18 @@
-# Temporary run function to make testing possible
-# By actully having a run function defined it can be called in a pipeline
+#!/usr/bin/env python3
+"""
+Remove the http-equiv="refresh" attribute from each meta element in which it is present.
+
+Type: Direct Function
+Info: https://dequeuniversity.com/rules/axe/3.2/meta-refresh
+"""
+
+from bs4 import BeautifulSoup
 
 
 def run(html):
+    # find all meta tags
+    for meta in html.find_all("meta"):
+        if meta.get("http-equiv") and meta["http-equiv"].lower() == "refresh":
+            del meta["http-equiv"]
+    # if present, remove the http-equiv="refresh" attribute
     return html
