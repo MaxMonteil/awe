328e96918db07f136be545a7e17ff94e40867ee8
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-01 18:49:42 +0300

    Implement meta-refresh function

--- engine/functions/meta_refresh/meta_refresh.py
+++ engine/functions/meta_refresh/meta_refresh.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-Remove the http-equiv="refresh" attribute from each meta element in which it is present.
+Remove meta tags with the http-equiv="refresh" attribute.
 
 Type: Direct Function
 Info: https://dequeuniversity.com/rules/axe/3.2/meta-refresh
+Procedure taken from: https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20161007/F41
 """
-
-from bs4 import BeautifulSoup
 
 
 def run(html):
     # find all meta tags
     for meta in html.find_all("meta"):
-        if meta.get("http-equiv") and meta["http-equiv"].lower() == "refresh":
-            del meta["http-equiv"]
-    # if present, remove the http-equiv="refresh" attribute
+        # if present, remove the http-equiv="refresh" attribute
+        if (meta.get("http-equiv") and meta["http-equiv"].lower() == "refresh"):
+            meta.decompose()
+
     return html
