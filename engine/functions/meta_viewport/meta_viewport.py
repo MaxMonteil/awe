a9ccd619a42455b84e9050479d2e69aa1deb49a2
MaxHermez <Max.Hermez@hotmail.com>
2019-05-01 20:56:12 +0300

    Update function to use new pipeline format of args

--- engine/functions/meta_viewport/meta_viewport.py
+++ engine/functions/meta_viewport/meta_viewport.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
-def run(html):
+
+def run(tag_data):
     """
     Ensures that the user-scalable="no" parameter is not present in 
     the <meta name="viewport"> element and the maximum-scale parameter 
@@ -11,23 +12,22 @@ def run(html):
     Return:
         <list> List of beautiful soup proper <meta> tags
     """
-    htmlTags = [BeautifulSoup(item["snippet"], "html.parser").find()
-                for item in html]
+    snippet = tag_data["snippet"]
     out = []
-    for tag in htmlTags:
-        # Remove "user-scalable=no if exists in the "content" attribute
-        if "user-scalable" in tag["content"]:
-            start = tag["content"].find("user-scalable")
-            end = tag["content"].find("no", start) + 2
-            end += 1 if tag["content"][end+1] == "," else 0
-            tag["content"] = tag["content"].replace(tag["content"][start:end], "")
-        # Sets "maximum-scale" to be at least = 2
-        if not "maximum-scale" in tag["content"]:
-            tag["conent"] = tag["content"] + ", maximum-scale=2"
-        scaleIndex = tag["conent"].find("maximum-scale")+14
-        scale = int(scaleIndex)
-        scale = 2 if scale < 2 else scale
-        tag["content"] = tag["content"].replace(
-                tag["content"][scaleIndex:scaleIndex+1], scale)
-        out.append(tag)
-    return out
+    # Remove "user-scalable=no if exists in the "content" attribute
+    if "user-scalable" in snippet["content"]:
+        start = snippet["content"].find("user-scalable")
+        end = snippet["content"].find("no", start) + 2
+        end += 1 if snippet["content"][end+1] == "," else 0
+        snippet["content"] = snippet["content"].replace(
+            snippet["content"][start:end], "")
+    # Sets "maximum-scale" to be at least = 2
+    if not "maximum-scale" in snippet["content"]:
+        snippet["conent"] = snippet["content"] + ", maximum-scale=2"
+    scaleIndex = snippet["conent"].find("maximum-scale")+14
+    scale = int(scaleIndex)
+    scale = 2 if scale < 2 else scale
+    snippet["content"] = snippet["content"].replace(
+        snippet["content"][scaleIndex:scaleIndex+1], scale)
+    tag_data["snippet"] = snippet
+    return tag_data
