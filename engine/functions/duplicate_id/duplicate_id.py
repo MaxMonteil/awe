a7d87a8395113858e48fe83135d062c2785cad72
marina keyrouz <marinakayrouz@gmail.com>
2019-05-02 19:19:24 +0300

    initial function

--- engine/functions/duplicate_id/duplicate_id.py
+++ engine/functions/duplicate_id/duplicate_id.py
@@ -1,6 +1,14 @@
 # Temporary run function to make testing possible
 # By actully having a run function defined it can be called in a pipeline
 
+import csv
 
-def run(html):
-    return html
+def run(tag_data):
+    snippet = tag_data["snippet"]
+
+    # Modifications to the BeautifulSoup tag 
+    get_val = snippet["id"]
+    snippet['id']=get_val+'1'
+    
+    tag_data["snippet"] = snippet
+    return tag_data
