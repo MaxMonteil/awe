fe67af6fbba25fcd47fb6153ed4e5c53a59d9e3a
Maria Daou <mariadaou16@gmail.com>
2019-05-03 17:05:32 +0300

    Update valid_lang.py

--- engine/functions/valid_lang/valid_lang.py
+++ engine/functions/valid_lang/valid_lang.py
@@ -1,6 +1,7 @@
 # Temporary run function to make testing possible
 # By actully having a run function defined it can be called in a pipeline
-
+'''Translates images alt text to the language in the HTML lang attribute.'''
 
 def run(html):
     return html
+'''<input id=\"txtAdvancedSearch\" placeholder=\"Tell us what you want to find\" resultslimit=\"5\" lang=\"1\" startfromnodeid=\"1\" autocomplete=\"off\" type=\"text\" class=\"ui-autocomplete-input\">'''
