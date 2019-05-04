938d0fb00f724d91622aea42c3917ace22a68795
Waseem Alghali <alghaliwaseem@gmail.com>
2019-05-04 04:07:35 +0300

    Pure function

--- engine/functions/dlitem/dlitem.py
+++ engine/functions/dlitem/dlitem.py
@@ -1,6 +1,30 @@
-# Temporary run function to make testing possible
-# By actully having a run function defined it can be called in a pipeline
+from bs4 import BeautifulSoup
 
 
-def run(html):
-    return html
+def run(data):
+    """
+    Wraps <dd> and <dt> tags in <dl> tags.
+
+    Input:
+    Full HTML code <soup>
+    Output:
+    Full HTML code with fixed tags <soup>
+    """
+    soup = data.html
+    items(soup)
+
+def items(soup):
+    """
+    Finds <dt> and <dd> tags, makes <dl> a sibling, then inserts tags in <dl> tag.
+    Input:
+    Full HTML code <soup>
+    Output:
+    Full HTML code with fixed tags <soup>
+    """
+    tags = soup.find_all(['dt', 'dd'])
+    dltag = BeautifulSoup("<dl></dl>","html.parser").find()
+    (tags[0].parent).append(dltag)
+    for tag in tags:
+        dltag.append(tag)
+    print(dltag.parent.parent)
+    return soup
